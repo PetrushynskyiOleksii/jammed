8914b7fcd1e50cb4e76501eb3ea74117dbe16c4c
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2019-07-14 21:47:22 +0300

    Create compiler for gtfs data using protobuf

---
+++ jammed/utils/easyway_helpers.py
@@ -0,0 +1,34 @@
+"""This module provides functionality to work with data from EasyWay."""
+
+from google import protobuf
+from google.transit import gtfs_realtime_pb2
+
+
+def compile_gtfs(gtfs):
+    """Compile GTFS data to dictionary format."""
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(gtfs)
+
+    gtfs_dict = {}
+    for entity in feed.entity:
+        vehicle = entity.vehicle
+        position = vehicle.position
+        route_id = vehicle.trip.route_id
+
+        if not gtfs_dict.get(route_id):
+            gtfs_dict[route_id] = []
+
+        gtfs_dict[route_id].append({
+            'route_id': route_id,
+            'timestamp': vehicle.timestamp,
+            'trip_id': vehicle.trip.trip_id,
+            'vehicle_id': vehicle.vehicle.id,
+            'license_plate': vehicle.vehicle.license_plate,
+            'latitude': position.latitude,
+            'longitude': position.longitude,
+            'odometer': position.odometer,
+            'bearing': position.bearing,
+            'speed': position.speed * 3.6,
+        })
+
+    return gtfs_dict
