aa176c6e8cafd9ddefca751ab7ac51bce45c1eec
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2019-07-28 11:23:18 +0300

    Add directory for dumped collected gtfs data

--- jammed/utils/constants.py
+++ jammed/utils/constants.py
@@ -5,6 +5,7 @@ import os
 
 BASE_DIR = os.path.abspath('..')
 PROJECT_DIR = os.path.abspath('.')
+COLLECTED_DIR = f'{BASE_DIR}/var/collected'
 EASYWAY_STATIC_DIR = f'{PROJECT_DIR}/static/ew'
 ROUTES_COLLECTION = 'routes'
 VEHICLE_URL = 'http://track.ua-gis.com/gtfs/lviv/vehicle_position'
