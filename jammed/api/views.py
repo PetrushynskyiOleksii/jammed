594a97662ce663fd9bab7a61eea84bcc7d59ea06
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2020-01-05 11:53:03 +0200

    Disable caching for bad requests

--- jammed/api/views.py
+++ jammed/api/views.py
@@ -14,17 +14,21 @@ def get_static_data():
     data_limit = request.args.get('limit', type=int, default=0)
     data_skip = request.args.get('skip', type=int, default=0)
 
-    documents = MONGER.find(
-        data_id,
-        fields={'_id': 0},
-        order_by=[('value', -1)],
-        limit=data_limit,
-        skip=data_skip
-    )
+    try:
+        documents = MONGER.find(
+            data_id,
+            fields={'_id': 0},
+            order_by=[('value', -1)],
+            limit=data_limit,
+            skip=data_skip
+        )
+    except AttributeError:
+        message = "Could not connect to database."
+        return jsonify({"error": message}), 400
 
     response = jsonify(list(documents))
     response.cache_control.public = True
-    response.cache_control.max_age = 604800  # week in seconds
+    response.cache_control.max_age = 86400  # day in seconds
     return response
 
 
@@ -32,6 +36,11 @@ def get_static_data():
 def get_static_data_count():
     """Return count of documents in certain collection."""
     data_id = request.args.get('id')
-    documents = MONGER.find(data_id)
+    try:
+        documents = MONGER.find(data_id)
+    except AttributeError:
+        message = "Could not connect to database."
+        return jsonify({"error": message}), 400
+
     response = jsonify({"count": documents.count()})
     return response
